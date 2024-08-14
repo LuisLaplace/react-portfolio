c952da86d61fca36ac316c7e2ed579a39b74258e
Laplace <lap2837@outlook.com>
2024-08-13 21:41:08 -0600

    added live to projecst

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,10 +1,10 @@
 import Project from './Project';
 
 const projects = [
-  { title: 'Ecommerce Back End', repo: 'https://github.com/LuisLaplace/e-commerce-back-end.git' },
+  { title: 'Ecommerce Back End', live: 'https://luislaplace.github.io/e-commerce-back-end/', repo: 'https://github.com/LuisLaplace/e-commerce-back-end.git' },
   { title: 'Nest Cast', img: '/img/NestCast.JPG', live: 'https://mpredelus3.github.io/nest-cast/home.html', repo: 'https://github.com/mpredelus3/nest-cast'},
-  { title: 'Task Board', repo: 'https://github.com/LuisLaplace/white-board' },
-  { title: 'Employee Payroll Tracker', repo: 'https://github.com/LuisLaplace/employee_rollcall' }
+  { title: 'Task Board', live: 'https://luislaplace.github.io/white-board/', repo: 'https://github.com/LuisLaplace/white-board' },
+  { title: 'Employee Payroll Tracker', live: 'https://luislaplace.github.io/employee_rollcall/', repo: 'https://github.com/LuisLaplace/employee_rollcall' }
 ];
 
 const Portfolio = () => (
